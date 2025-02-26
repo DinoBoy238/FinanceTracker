ae3083c3c7a52e0163e5204f0a4db3fed4a5b3e3
Spencer <spencerfbrandon@gmail.com>
2025-02-26 10:11:24 -0500

    Explained gameplan in a comment

--- FinanceTracker.js
+++ FinanceTracker.js
@@ -1,1 +1,1 @@
-// JavaScript Document
+/*Make an array for all of the days in the calendar and then have the td or tr:nth-child(x) in it for each position in the calendar so you can easily just acceses the array to find a postion based on its nth day, and have a starting varible so when you have day 1 start like 3 days in because its a teusday you just add or subtract that to the array calculatitons */
