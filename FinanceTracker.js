3c12ae98e0f701fee9e6800ecc94ff4a8c5d42bb
Spencer <spencerfbrandon@gmail.com>
2025-03-03 10:35:07 -0500

    Added an array that has every day in it

--- FinanceTracker.js
+++ FinanceTracker.js
@@ -1,12 +1,12 @@
 /*Make an array for all of the days in the calendar and then have the td or tr:nth-child(x) in it for each position in the calendar so you can easily just acceses the array to find a postion based on its nth day, and have a starting varible so when you have day 1 start like 3 days in because its a teusday you just add or subtract that to the array calculatitons */
+//Waiting for page to load because some varibles need the content of page
 document.addEventListener("DOMContentLoaded", function() {
-    var cell = [document.querySelector("tr:nth-child(2) td:nth-child(1)")];
-
-    if (cell[0]) {
-        console.log(cell[0].innerHTML);
-    } else {
-        console.log("The element wasn't found.");
-    }
+var days = [/*Number(document.querySelector("tr:nth-child(2) td:nth-child(1)").innerHTML)*/];
+for (var i = 2; i < 8; i++){
+	for (var y = 1; y < 8; y++) {
+		//Added backticks to properly interpolate the varible into the string
+		days.push(Number(document.querySelector(`tr:nth-child(${i}) td:nth-child(${y})`).innerHTML));
+	}
+}
+console.log(days);
 });
-var days = [document.querySelector("tr:nth-child(2) td:nth-child(1)")];
-console.log(days[0]);
